06e0cb62b56d5efac5fa70e014268c122c25ae29
Samara Lipolis <samararochalipolis@gmail.com>
2018-08-29 13:29:54 -0300

    .

--- app/controllers/HomeController.js
+++ app/controllers/HomeController.js
@@ -2,17 +2,29 @@ app.controller('HomeController', function($rootScope, $scope, $location, $http)
 {
   $rootScope.uri = "https://apiestoqueando.herokuapp.com";
   $scope.consultantData = {};
-  
+  $scope.loading = false;
+  $scope.success = false;
+
+
    $scope.addConsultant = function() {
-       
-    $http.post( $rootScope.uri+ '/consultant/add', {'consultantData': $scope.consultantData }, 
+    $scope.loading = true;
+
+    $http.post( $rootScope.uri+ '/consultant/add',{'consultantData': $scope.consultantData }, 
     {
         headers: {'Content-Type': 'application/json'} 
     })
     .success(function (data, status, headers, config) {
+        $scope.loading = false;
+        $scope.success = true;
+
     })
     .error(function (data, status, header, config) {
+        $scope.loading = false;
+        $scope.success = false;
+
+
     });
 
     }
+
 });
